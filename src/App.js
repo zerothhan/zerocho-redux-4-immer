e64584ae1a6c55ffdeb04b774f07653ac338f192
Juhwa Hwang <zerothhan@gmail.com>
2022-08-26 11:30:55 +0900

    redux class

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useCallback } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import { logIn, logOut } from "./redux/actions/user";
 
-function App() {
-  const user = useSelector((state) => state.user);
-  const dispatch = useDispatch();
+class App {
+  onClick = () => {
+    this.props.dispatchLogIn({
+      id: "zerocho",
+      password: "1234",
+    });
+  };
 
-  const onClick = useCallback(() => {
-    dispatch(
-      logIn({
-        id: "zerocho",
-        password: "1234",
-      })
+  onLogout = () => {
+    this.props.dispatchLogOut();
+  };
+
+  render() {
+    const { user } = this.props;
+    return (
+      <div>
+        {user.isLoggingIn ? (
+          <div>로그인 중</div>
+        ) : user.data ? (
+          <div>{user.data.nickname}</div>
+        ) : (
+          "로그인 해주세요."
+        )}
+        {!user.data ? (
+          <button onClick={onClick}>로그인</button>
+        ) : (
+          <button onClick={onLogout}>로그아웃</button>
+        )}
+      </div>
     );
-  }, []);
-
-  const onLogout = useCallback(() => {
-    dispatch(logOut());
-  }, []);
-
-  return (
-    <div>
-      {user.isLoggingIn ? (
-        <div>로그인 중</div>
-      ) : user.data ? (
-        <div>{user.data.nickname}</div>
-      ) : (
-        "로그인 해주세요."
-      )}
-      {!user.data ? (
-        <button onClick={onClick}>로그인</button>
-      ) : (
-        <button onClick={onLogout}>로그아웃</button>
-      )}
-    </div>
-  );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  user: state.user,
+  posts: state.posts,
+}); // reselect
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchLogIn: (data) => dispatch(logIn(data)),
+  dispatchLogOut: () => dispatch(logOut()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
